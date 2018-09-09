797eb993bd96307a8fb5962e448123bc91624ed2
yaoying <yaoying@meitunmama.com>
2018-09-09 22:05:05 +0800

    Revert "test commit"
    This reverts commit 5a31e4cefff74410a712f3846ae668ee0d4e322f.

--- firstlanguage.m
+++ firstlanguage.m
@@ -2,7 +2,6 @@
 #import "AppDelegate.h"
 
 int main(int argc, char * argv[]) {
-	NSInteger test a = 5;
     @autoreleasepool {
     	NSInteger testFloat = 6.0;
         NSInteger testInterger = 7.0f;
