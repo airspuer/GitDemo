f7706378086691fa852b043d8754aefc95dae9be
airspuer <286436221@qq.com>
2018-09-12 23:58:22 +0800

    amend firstlanguage.m

--- firstlanguage.m
+++ firstlanguage.m
@@ -3,6 +3,8 @@
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+    	NSInteger testFloat = 6.0;
+        NSInteger testInterger = 7.0f;
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
--- firstlanguage2.m
+++ firstlanguage2.m
@@ -4,6 +4,8 @@
 int main(int argc, char * argv[]) {
     @autoreleasepool {
     	NSInteger testNumber = 5;
+    	NSLog("%@",@(testNumber));
+    	
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
