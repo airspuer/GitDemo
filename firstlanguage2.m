855f56d4c7cbc85816a2475c2f4d6d851edbc07a
airspuer <286436221@qq.com>
2018-09-12 23:40:26 +0800

    add testNumber

--- firstlanguage2.m
+++ firstlanguage2.m
@@ -3,6 +3,7 @@
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+    	NSInteger testNumber = 5;
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
