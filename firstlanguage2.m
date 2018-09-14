14b84e80abd10d726f99419eed3502f68dfcaf1d
airspuer <286436221@qq.com>
2018-09-15 00:12:52 +0800

    修改 language2

--- firstlanguage2.m
+++ firstlanguage2.m
@@ -4,8 +4,6 @@
 int main(int argc, char * argv[]) {
     @autoreleasepool {
     	NSInteger testNumber = 5;
-    	NSLog("%@",@(testNumber));
-    	
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
