1c55ae2c8bf61deee40bf5fc44eb72bc5a05bddb
yaoying <yaoying@meitunmam.com>
2018-09-16 18:04:03 +0800

    admend file2

--- firstlanguage2.m
+++ firstlanguage2.m
@@ -3,8 +3,12 @@
 
 int main(int argc, char * argv[]) {
     @autoreleasepool {
+
     	NSInterger testInt = 9;
-    	
+    	NSInterger testInt2 = 10;
+
+    	NSInteger testNum = 6;
+
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
